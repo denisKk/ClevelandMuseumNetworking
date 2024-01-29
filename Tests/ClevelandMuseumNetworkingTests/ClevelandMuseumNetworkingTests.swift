f26fb9c0807d7be34f437294f370e670fac560de
Denis Kulakov <kulakovdenis@mail.ru>
2024-01-29 20:22:56 +0300

    comment  '@available'

--- Tests/ClevelandMuseumNetworkingTests/ClevelandMuseumNetworkingTests.swift
+++ Tests/ClevelandMuseumNetworkingTests/ClevelandMuseumNetworkingTests.swift
@@ -6,6 +6,6 @@ final class ClevelandMuseumNetworkingTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ClevelandMuseumNetworking().text, "Hello, World!")
+//        XCTAssertEqual(ClevelandMuseumNetworking().text, "Hello, World!")
     }
 }
